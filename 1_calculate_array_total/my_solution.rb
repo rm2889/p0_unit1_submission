1489658340bf93bbdbd99f973235dd3a81f7725d
Raghav Malik <raghav1987@gmail.com>
2014-04-17 00:34:16 -0400

    finished first two problems

--- 1_calculate_array_total/my_solution.rb
+++ 1_calculate_array_total/my_solution.rb
@@ -4,22 +4,90 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
 # What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# 	Input is an array of numbers
 
+# 	Input can be an array of strings or numbers
+
+# What is the output? (i.e. What should the code return?)
+# 	Outpus is a single number that represents their sum
+
+# 	Output will be a string
+
+# What are the steps needed to solve the problem?
+=begin 
+	Set runningTotal = 0
+	FOR each element in input_array
+		runningTotal = runningTotal + element
+	END FOR
+	return runningTotal
+=end
+
+=begin
+	Set sentence = ""
+	Set length = input_array.length
+	Set i = 0
+	WHILE i < length
+		IF i = 0
+			sentence += input_array[i].capitalize
+		ELSE
+			sentence += " " + input_array[i]
+		END IF
+		increment i by 1
+	END WHILE
+	sentence += "."
+	return sentence
+=end	
 
 # 2. Initial Solution
 
+def total (input_array)
+	running_total = 0
+	input_array.each { |x|
+		running_total+=x
+	}
+	return running_total
+end
 
+def sentence_maker (input_array)
+	sentence = ""
+	length = input_array.length
+	i = 0
+	while i < length
+		if i == 0
+			sentence += input_array[i].to_s.capitalize
+		else
+			sentence += " " + input_array[i].to_s
+		end
+		i+=1
+	end
+	sentence += "."
+	return sentence	
+end
 
 
 # 3. Refactored Solution
+=begin
+	I think the solution for total is pretty good - I really can't think of a better way to do this. Every variable declared
+	has a purpose.
 
-
+	The solution to sentence_maker can be a bit better but I'm not really sure - for instance, at every iteration of the loop 
+	I'm checking if the index is the first index. Another way to implement this method could be I remove the first element of
+	the array and store it in another variable -> like a pop or push or whatever that array method is, and then loop through
+	the array without having to perform this if condition at every iteration. That might be less expensive computationally.
+	
+=end
 
 # 4. Reflection 
+=begin
+	While implementing the second method, I encountered a couple of errors - the first one was handling number arguments, which
+	didn't work with the + operator. Another mistake I made was getting my while condition to be while i < length - 1, where
+	i should've done i < lenght or i <= length - 1
+
+
+
+=end
--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -9,11 +9,48 @@
 # 1. Pseudocode
 
 # What is the input?
+# 	Array of numbers
 # What is the output? (i.e. What should the code return?)
+# 	a single letter
 # What are the steps needed to solve the problem?
 
+=begin
+	same pseudocode as total in the last exercise, except in this case 
+	convert the sum to float and divide by the length to get average
+
+	Set average = total/input_array.length
+	IF average >= 90
+		grade = "A"
+	ELSE IF average >= 80
+		grade = "B"
+	...
+	ELSE 
+		grade = "F"
+	end
+=end
 
 # 2. Initial Solution
+
+def get_grade (input_array)
+	running_total = 0
+	input_array.each { |x|
+		running_total+=x
+	}
+	average = running_total.to_f/input_array.length
+	if average >= 90
+		return "A"
+	elsif average >= 80
+		return "B"
+	elsif average >= 70
+		return "C"
+	elsif average >= 60
+		return "D"
+	elsif average >= 50
+		return "E"
+	else 
+		return "F"
+	end
+end
 
 
 
@@ -21,5 +58,5 @@
 # 3. Refactored Solution
 
 
-
 # 4. Reflection 
+#  I looked for an in built array function that could calculate the average, only to remember how simple the calculation is!
